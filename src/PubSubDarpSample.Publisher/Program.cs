7b83bb6f8760d40f63dca1382f6c097deb7eda20
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2022-04-29 07:50:35 +0200

    Add good config to Secret.json
    Add good config to Secret.json

--- src/PubSubDarpSample.Publisher/Program.cs
+++ src/PubSubDarpSample.Publisher/Program.cs
@@ -2,10 +2,11 @@
 
 using PubSubDarpSample.Publisher;
 
-await GenerateGuestActionsMessages("pubisher-microservice", "mydaprdemoeventhub");
+await GenerateGuestActionsMessages("pub-microservice", "mydaprdemoeventhub");
 
 async Task GenerateGuestActionsMessages(string pubSubName, string topic)
 {
     var guestPubService = new EventService();
     await guestPubService.SendEventMessages(2, pubSubName, topic);
+    //var testSecretVar = "";
 }
